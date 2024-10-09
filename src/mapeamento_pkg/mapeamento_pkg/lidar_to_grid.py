bcc101e3d4f4f0bd56ad029df7b27d2caa3a6c2a
vmayres <victoroliveiraayres@gmail.com>
2024-10-09 17:29:52 -0300

    poraaaaaaaa

--- src/mapeamento_pkg/mapeamento_pkg/lidar_to_grid.py
+++ src/mapeamento_pkg/mapeamento_pkg/lidar_to_grid.py
@@ -21,13 +21,13 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 import tf_transformations
 
-import lidar_to_grid_map as lg
+#import lidar_to_grid_map as lg
 from collections import deque
 
 EXTEND_AREA = 1.0
 
-
 class mapa(Node):
+
 
     # Construtor do nó
     def __init__(self):
@@ -62,6 +62,216 @@ class mapa(Node):
 
     def listener_callback_odom(self, msg):
         self.pose = msg.pose.pose
+
+        # função de callback do timer
+    def on_timer(self):
+        try:
+            self.tf_right = self.tf_buffer.lookup_transform(
+                "right_center_wheel",
+                "right_leg_base",
+                rclpy.time.Time())
+
+            _, _, self.right_yaw = tf_transformations.euler_from_quaternion(
+                [self.tf_right.transform.rotation.x, self.tf_right.transform.rotation.y, 
+                 self.tf_right.transform.rotation.z, self.tf_right.transform.rotation.w])
+
+            self.get_logger().info(
+                f'yaw right_leg_base to right_center_wheel: {self.right_yaw}')
+
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform right_leg_base to right_center_wheel: {ex}')
+
+        try:
+            self.tf_left = self.tf_buffer.lookup_transform(
+                "left_center_wheel",
+                "left_leg_base",
+                rclpy.time.Time())
+
+            _, _, self.left_yaw = tf_transformations.euler_from_quaternion(
+                [self.tf_left.transform.rotation.x, self.tf_left.transform.rotation.y,
+                 self.tf_left.transform.rotation.z, self.tf_left.transform.rotation.w])
+
+            self.get_logger().info(
+                f'yaw left_leg_base to left_center_wheel: {self.left_yaw}')
+
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform left_leg_base to left_center_wheel: {ex}')
+
+    @staticmethod
+    def bresenham(start, end):
+        """
+        Implementation of Bresenham's line drawing algorithm
+        See en.wikipedia.org/wiki/Bresenham's_line_algorithm
+        Bresenham's Line Algorithm
+        Produces a np.array from start and end (original from roguebasin.com)
+        >>> points1 = bresenham((4, 4), (6, 10))
+        >>> print(points1)
+        np.array([[4,4], [4,5], [5,6], [5,7], [5,8], [6,9], [6,10]])
+        """
+        # setup initial conditions
+        x1, y1 = start
+        x2, y2 = end
+        dx = x2 - x1
+        dy = y2 - y1
+        is_steep = abs(dy) > abs(dx)  # determine how steep the line is
+        if is_steep:  # rotate line
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+        # swap start and end points if necessary and store swap state
+        swapped = False
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+            swapped = True
+        dx = x2 - x1  # recalculate differentials
+        dy = y2 - y1  # recalculate differentials
+        error = int(dx / 2.0)  # calculate error
+        y_step = 1 if y1 < y2 else -1
+        # iterate over bounding box generating points between start and end
+        y = y1
+        points = []
+        for x in range(x1, x2 + 1):
+            coord = [y, x] if is_steep else (x, y)
+            points.append(coord)
+            error -= abs(dy)
+            if error < 0:
+                y += y_step
+                error += dx
+        if swapped:  # reverse the list if the coordinates were swapped
+            points.reverse()
+        points = np.array(points)
+        return points
+
+    @staticmethod
+    def calc_grid_map_config(ox, oy, xy_resolution):
+        """
+        Calculates the size, and the maximum distances according to the the
+        measurement center
+        """
+        min_x = round(min(ox) - EXTEND_AREA / 2.0)
+        min_y = round(min(oy) - EXTEND_AREA / 2.0)
+        max_x = round(max(ox) + EXTEND_AREA / 2.0)
+        max_y = round(max(oy) + EXTEND_AREA / 2.0)
+        xw = int(round((max_x - min_x) / xy_resolution))
+        yw = int(round((max_y - min_y) / xy_resolution))
+        print("The grid map is ", xw, "x", yw, ".")
+        return min_x, min_y, max_x, max_y, xw, yw
+
+    @staticmethod
+    def atan_zero_to_twopi(y, x):
+        angle = math.atan2(y, x)
+        if angle < 0.0:
+            angle += math.pi * 2.0
+        return angle
+
+    @staticmethod
+    def init_flood_fill(center_point, obstacle_points, xy_points, min_coord,
+                        xy_resolution):
+        """
+        center_point: center point
+        obstacle_points: detected obstacles points (x,y)
+        xy_points: (x,y) point pairs
+        """
+        center_x, center_y = center_point
+        prev_ix, prev_iy = center_x - 1, center_y
+        ox, oy = obstacle_points
+        xw, yw = xy_points
+        min_x, min_y = min_coord
+        occupancy_map = (np.ones((xw, yw))) * 0.5
+        for (x, y) in zip(ox, oy):
+            # x coordinate of the the occupied area
+            ix = int(round((x - min_x) / xy_resolution))
+            # y coordinate of the the occupied area
+            iy = int(round((y - min_y) / xy_resolution))
+            free_area = bresenham((prev_ix, prev_iy), (ix, iy))
+            for fa in free_area:
+                occupancy_map[fa[0]][fa[1]] = 0  # free area 0.0
+            prev_ix = ix
+            prev_iy = iy
+        return occupancy_map
+
+    @staticmethod
+    def flood_fill(center_point, occupancy_map):
+        """
+        center_point: starting point (x,y) of fill
+        occupancy_map: occupancy map generated from Bresenham ray-tracing
+        """
+        # Fill empty areas with queue method
+        sx, sy = occupancy_map.shape
+        fringe = deque()
+        fringe.appendleft(center_point)
+        while fringe:
+            n = fringe.pop()
+            nx, ny = n
+            # West
+            if nx > 0:
+                if occupancy_map[nx - 1, ny] == 0.5:
+                    occupancy_map[nx - 1, ny] = 0.0
+                    fringe.appendleft((nx - 1, ny))
+            # East
+            if nx < sx - 1:
+                if occupancy_map[nx + 1, ny] == 0.5:
+                    occupancy_map[nx + 1, ny] = 0.0
+                    fringe.appendleft((nx + 1, ny))
+            # North
+            if ny > 0:
+                if occupancy_map[nx, ny - 1] == 0.5:
+                    occupancy_map[nx, ny - 1] = 0.0
+                    fringe.appendleft((nx, ny - 1))
+            # South
+            if ny < sy - 1:
+                if occupancy_map[nx, ny + 1] == 0.5:
+                    occupancy_map[nx, ny + 1] = 0.0
+                    fringe.appendleft((nx, ny + 1))
+
+    @staticmethod
+    def generate_ray_casting_grid_map(ox, oy, xy_resolution, breshen=True):
+        """
+        The breshen boolean tells if it's computed with bresenham ray casting
+        (True) or with flood fill (False)
+        """
+        min_x, min_y, max_x, max_y, x_w, y_w = calc_grid_map_config(
+            ox, oy, xy_resolution)
+        # default 0.5 -- [[0.5 for i in range(y_w)] for i in range(x_w)]
+        occupancy_map = np.ones((x_w, y_w)) / 2
+        center_x = int(
+            round(-min_x / xy_resolution))  # center x coordinate of the grid map
+        center_y = int(
+            round(-min_y / xy_resolution))  # center y coordinate of the grid map
+        # occupancy grid computed with bresenham ray casting
+        if breshen:
+            for (x, y) in zip(ox, oy):
+                # x coordinate of the the occupied area
+                ix = int(round((x - min_x) / xy_resolution))
+                # y coordinate of the the occupied area
+                iy = int(round((y - min_y) / xy_resolution))
+                laser_beams = bresenham((center_x, center_y), (
+                    ix, iy))  # line form the lidar to the occupied point
+                for laser_beam in laser_beams:
+                    occupancy_map[laser_beam[0]][
+                        laser_beam[1]] = 0.0  # free area 0.0
+                occupancy_map[ix][iy] = 1.0  # occupied area 1.0
+                occupancy_map[ix + 1][iy] = 1.0  # extend the occupied area
+                occupancy_map[ix][iy + 1] = 1.0  # extend the occupied area
+                occupancy_map[ix + 1][iy + 1] = 1.0  # extend the occupied area
+        # occupancy grid computed with with flood fill
+        else:
+            occupancy_map = init_flood_fill((center_x, center_y), (ox, oy),
+                                            (x_w, y_w),
+                                            (min_x, min_y), xy_resolution)
+            flood_fill((center_x, center_y), occupancy_map)
+            occupancy_map = np.array(occupancy_map, dtype=float)
+            for (x, y) in zip(ox, oy):
+                ix = int(round((x - min_x) / xy_resolution))
+                iy = int(round((y - min_y) / xy_resolution))
+                occupancy_map[ix][iy] = 1.0  # occupied area 1.0
+                occupancy_map[ix + 1][iy] = 1.0  # extend the occupied area
+                occupancy_map[ix][iy + 1] = 1.0  # extend the occupied area
+                occupancy_map[ix + 1][iy + 1] = 1.0  # extend the occupied area
+        return occupancy_map, min_x, max_x, min_y, max_y, xy_resolution
+
 
     def flood_fill(self, cpoint, pmap):
         """
@@ -116,7 +326,7 @@ class mapa(Node):
         xyreso = 0.02  # x-y grid resolution
         yawreso = math.radians(3.1)  # yaw angle resolution [rad]
 
-        pmap, minx, maxx, miny, maxy, xyreso = lg.generate_ray_casting_grid_map(ox, oy, xyreso, False)
+        pmap, minx, maxx, miny, maxy, xyreso = generate_ray_casting_grid_map(ox, oy, xyreso, False)
         xyres = np.array(pmap).shape
 
         # Plot the laser map
@@ -130,6 +340,7 @@ class mapa(Node):
         plt.colorbar()
         plt.show()
 
+    # TODO create the update function 
     def update():
         pass
 
@@ -143,7 +354,7 @@ class mapa(Node):
 
             self.laser_map()
 
-            #create the fist map
+            # TODO create the fist map
 
             #update the map
             self.update()
--- src/mapeamento_pkg/mapeamento_pkg/lidar_to_grid_map.py
+++ src/mapeamento_pkg/mapeamento_pkg/lidar_to_grid_map.py
@@ -1,22 +1,36 @@
 """
+
 LIDAR to 2D grid map example
 
 author: Erno Horvath, Csaba Hajdu based on Atsushi Sakai's scripts
+
 """
 
 import math
 from collections import deque
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+EXTEND_AREA = 1.0
 
-class LidarToGridMap:
-    EXTEND_AREA = 1.0
 
-    def __init__(self):
-        pass
+def file_read(f):
+    """
+    Reading LIDAR laser beams (angles and corresponding distance data)
+    """
+    with open(f) as data:
+        measures = [line.split(",") for line in data]
+    angles = []
+    distances = []
+    for measure in measures:
+        angles.append(float(measure[0]))
+        distances.append(float(measure[1]))
+    angles = np.array(angles)
+    distances = np.array(distances)
+    return angles, distances
 
-    @staticmethod
+
     def bresenham(start, end):
         """
         Implementation of Bresenham's line drawing algorithm
@@ -32,114 +46,195 @@ class LidarToGridMap:
         x2, y2 = end
         dx = x2 - x1
         dy = y2 - y1
-
-        is_steep = abs(dy) > abs(dx)
-
-        if is_steep:
+        is_steep = abs(dy) > abs(dx)  # determine how steep the line is
+        if is_steep:  # rotate line
             x1, y1 = y1, x1
             x2, y2 = y2, x2
-
+        # swap start and end points if necessary and store swap state
         swapped = False
         if x1 > x2:
             x1, x2 = x2, x1
             y1, y2 = y2, y1
             swapped = True
-
-        dx = x2 - x1
-        dy = y2 - y1
-
-        error = int(dx / 2.0)
-        ystep = 1 if y1 < y2 else -1
-
+        dx = x2 - x1  # recalculate differentials
+        dy = y2 - y1  # recalculate differentials
+        error = int(dx / 2.0)  # calculate error
+        y_step = 1 if y1 < y2 else -1
+        # iterate over bounding box generating points between start and end
         y = y1
         points = []
         for x in range(x1, x2 + 1):
-            coord = (y, x) if is_steep else (x, y)
+            coord = [y, x] if is_steep else (x, y)
             points.append(coord)
             error -= abs(dy)
             if error < 0:
-                y += ystep
+                y += y_step
                 error += dx
-
-        if swapped:
+        if swapped:  # reverse the list if the coordinates were swapped
             points.reverse()
-        return np.array(points)
+        points = np.array(points)
+        return points
 
-    @staticmethod
-    def calc_grid_map_config(ox, oy, xyreso):
-        minx = round(min(ox) - LidarToGridMap.EXTEND_AREA / 2.0)
-        miny = round(min(oy) - LidarToGridMap.EXTEND_AREA / 2.0)
-        maxx = round(max(ox) + LidarToGridMap.EXTEND_AREA / 2.0)
-        maxy = round(max(oy) + LidarToGridMap.EXTEND_AREA / 2.0)
-        xw = int(round((maxx - minx) / xyreso))
-        yw = int(round((maxy - miny) / xyreso))
 
-        return minx, miny, maxx, maxy, xw, yw
+    def calc_grid_map_config(ox, oy, xy_resolution):
+        """
+        Calculates the size, and the maximum distances according to the the
+        measurement center
+        """
+        min_x = round(min(ox) - EXTEND_AREA / 2.0)
+        min_y = round(min(oy) - EXTEND_AREA / 2.0)
+        max_x = round(max(ox) + EXTEND_AREA / 2.0)
+        max_y = round(max(oy) + EXTEND_AREA / 2.0)
+        xw = int(round((max_x - min_x) / xy_resolution))
+        yw = int(round((max_y - min_y) / xy_resolution))
+        print("The grid map is ", xw, "x", yw, ".")
+        return min_x, min_y, max_x, max_y, xw, yw
 
-    @staticmethod
-    def init_flood_fill(centerpoint, obstacle_points, minx, maxx, miny, maxy, xyreso):
-        w, h = maxx - minx, maxy - miny
-        grid = np.ones((w, h)) * 0.5
-        for (x, y) in obstacle_points:
-            ix = int(round((x - minx) / xyreso))
-            iy = int(round((y - miny) / xyreso))
-            grid[ix][iy] = 0.0
 
-        center_x = int(round((centerpoint[0] - minx) / xyreso))
-        center_y = int(round((centerpoint[1] - miny) / xyreso))
+    def atan_zero_to_twopi(y, x):
+        angle = math.atan2(y, x)
+        if angle < 0.0:
+            angle += math.pi * 2.0
+        return angle
 
-        return grid, center_x, center_y
 
-    @staticmethod
-    def flood_fill(grid, x, y):
-        stack = deque([(x, y)])
-        while stack:
-            n = stack.pop()
-            if grid[n[0]][n[1]] == 0.5:
-                grid[n[0]][n[1]] = 1.0
-                if n[0] > 0:
-                    stack.append((n[0] - 1, n[1]))
-                if n[0] < grid.shape[0] - 1:
-                    stack.append((n[0] + 1, n[1]))
-                if n[1] > 0:
-                    stack.append((n[0], n[1] - 1))
-                if n[1] < grid.shape[1] - 1:
-                    stack.append((n[0], n[1] + 1))
-
-    @staticmethod
-    def generate_ray_casting_grid_map(ox, oy, xyreso, show_animation=False):
-        minx, miny, maxx, maxy, xw, yw = LidarToGridMap.calc_grid_map_config(
-            ox, oy, xyreso)
-
-        pmap = np.ones((xw, yw)) * 0.5
-
+    def init_flood_fill(center_point, obstacle_points, xy_points, min_coord,
+                        xy_resolution):
+        """
+        center_point: center point
+        obstacle_points: detected obstacles points (x,y)
+        xy_points: (x,y) point pairs
+        """
+        center_x, center_y = center_point
+        prev_ix, prev_iy = center_x - 1, center_y
+        ox, oy = obstacle_points
+        xw, yw = xy_points
+        min_x, min_y = min_coord
+        occupancy_map = (np.ones((xw, yw))) * 0.5
         for (x, y) in zip(ox, oy):
-            ix = int(round((x - minx) / xyreso))
-            iy = int(round((y - miny) / xyreso))
-            pmap[ix][iy] = 0.0
+            # x coordinate of the the occupied area
+            ix = int(round((x - min_x) / xy_resolution))
+            # y coordinate of the the occupied area
+            iy = int(round((y - min_y) / xy_resolution))
+            free_area = bresenham((prev_ix, prev_iy), (ix, iy))
+            for fa in free_area:
+                occupancy_map[fa[0]][fa[1]] = 0  # free area 0.0
+            prev_ix = ix
+            prev_iy = iy
+        return occupancy_map
 
-        for (x, y) in zip(ox, oy):
-            ix = int(round((x - minx) / xyreso))
-            iy = int(round((y - miny) / xyreso))
 
-            laser_beams = LidarToGridMap.bresenham(
-                (int((0.0 - minx) / xyreso), int((0.0 - miny) / xyreso)), (ix, iy))
+    def flood_fill(center_point, occupancy_map):
+        """
+        center_point: starting point (x,y) of fill
+        occupancy_map: occupancy map generated from Bresenham ray-tracing
+        """
+        # Fill empty areas with queue method
+        sx, sy = occupancy_map.shape
+        fringe = deque()
+        fringe.appendleft(center_point)
+        while fringe:
+            n = fringe.pop()
+            nx, ny = n
+            # West
+            if nx > 0:
+                if occupancy_map[nx - 1, ny] == 0.5:
+                    occupancy_map[nx - 1, ny] = 0.0
+                    fringe.appendleft((nx - 1, ny))
+            # East
+            if nx < sx - 1:
+                if occupancy_map[nx + 1, ny] == 0.5:
+                    occupancy_map[nx + 1, ny] = 0.0
+                    fringe.appendleft((nx + 1, ny))
+            # North
+            if ny > 0:
+                if occupancy_map[nx, ny - 1] == 0.5:
+                    occupancy_map[nx, ny - 1] = 0.0
+                    fringe.appendleft((nx, ny - 1))
+            # South
+            if ny < sy - 1:
+                if occupancy_map[nx, ny + 1] == 0.5:
+                    occupancy_map[nx, ny + 1] = 0.0
+                    fringe.appendleft((nx, ny + 1))
 
-            for laser_beam in laser_beams:
-                pmap[laser_beam[0]][laser_beam[1]] = 1.0
 
-        centerpoint = (0.0, 0.0)
-        grid, center_x, center_y = LidarToGridMap.init_flood_fill(
-            centerpoint, zip(ox, oy), minx, maxx, miny, maxy, xyreso)
-        LidarToGridMap.flood_fill(grid, center_x, center_y)
+    def generate_ray_casting_grid_map(ox, oy, xy_resolution, breshen=True):
+        """
+        The breshen boolean tells if it's computed with bresenham ray casting
+        (True) or with flood fill (False)
+        """
+        min_x, min_y, max_x, max_y, x_w, y_w = calc_grid_map_config(
+            ox, oy, xy_resolution)
+        # default 0.5 -- [[0.5 for i in range(y_w)] for i in range(x_w)]
+        occupancy_map = np.ones((x_w, y_w)) / 2
+        center_x = int(
+            round(-min_x / xy_resolution))  # center x coordinate of the grid map
+        center_y = int(
+            round(-min_y / xy_resolution))  # center y coordinate of the grid map
+        # occupancy grid computed with bresenham ray casting
+        if breshen:
+            for (x, y) in zip(ox, oy):
+                # x coordinate of the the occupied area
+                ix = int(round((x - min_x) / xy_resolution))
+                # y coordinate of the the occupied area
+                iy = int(round((y - min_y) / xy_resolution))
+                laser_beams = bresenham((center_x, center_y), (
+                    ix, iy))  # line form the lidar to the occupied point
+                for laser_beam in laser_beams:
+                    occupancy_map[laser_beam[0]][
+                        laser_beam[1]] = 0.0  # free area 0.0
+                occupancy_map[ix][iy] = 1.0  # occupied area 1.0
+                occupancy_map[ix + 1][iy] = 1.0  # extend the occupied area
+                occupancy_map[ix][iy + 1] = 1.0  # extend the occupied area
+                occupancy_map[ix + 1][iy + 1] = 1.0  # extend the occupied area
+        # occupancy grid computed with with flood fill
+        else:
+            occupancy_map = init_flood_fill((center_x, center_y), (ox, oy),
+                                            (x_w, y_w),
+                                            (min_x, min_y), xy_resolution)
+            flood_fill((center_x, center_y), occupancy_map)
+            occupancy_map = np.array(occupancy_map, dtype=float)
+            for (x, y) in zip(ox, oy):
+                ix = int(round((x - min_x) / xy_resolution))
+                iy = int(round((y - min_y) / xy_resolution))
+                occupancy_map[ix][iy] = 1.0  # occupied area 1.0
+                occupancy_map[ix + 1][iy] = 1.0  # extend the occupied area
+                occupancy_map[ix][iy + 1] = 1.0  # extend the occupied area
+                occupancy_map[ix + 1][iy + 1] = 1.0  # extend the occupied area
+        return occupancy_map, min_x, max_x, min_y, max_y, xy_resolution
 
-        for x in range(xw):
-            for y in range(yw):
-                if grid[x][y] == 1.0:
-                    pmap[x][y] = 1.0
 
-        if show_animation:
-            plt.imshow(pmap, cmap="PiYG_r")
-            plt.pause(1.0)
+def main():
+    """
+    Example usage
+    """
+    print(__file__, "start")
+    xy_resolution = 0.02  # x-y grid resolution
+    ang, dist = file_read("lidar01.csv")
+    ox = np.sin(ang) * dist
+    oy = np.cos(ang) * dist
+    occupancy_map, min_x, max_x, min_y, max_y, xy_resolution = \
+        generate_ray_casting_grid_map(ox, oy, xy_resolution, True)
+    xy_res = np.array(occupancy_map).shape
+    plt.figure(1, figsize=(10, 4))
+    plt.subplot(122)
+    plt.imshow(occupancy_map, cmap="PiYG_r")
+    # cmap = "binary" "PiYG_r" "PiYG_r" "bone" "bone_r" "RdYlGn_r"
+    plt.clim(-0.4, 1.4)
+    plt.gca().set_xticks(np.arange(-.5, xy_res[1], 1), minor=True)
+    plt.gca().set_yticks(np.arange(-.5, xy_res[0], 1), minor=True)
+    plt.grid(True, which="minor", color="w", linewidth=0.6, alpha=0.5)
+    plt.colorbar()
+    plt.subplot(121)
+    plt.plot([oy, np.zeros(np.size(oy))], [ox, np.zeros(np.size(oy))], "ro-")
+    plt.axis("equal")
+    plt.plot(0.0, 0.0, "ob")
+    plt.gca().set_aspect("equal", "box")
+    bottom, top = plt.ylim()  # return the current y-lim
+    plt.ylim((top, bottom))  # rescale y axis, to match the grid orientation
+    plt.grid(True)
+    plt.show()
 
-        return pmap, minx, maxx, miny, maxy, xyreso
+
+if __name__ == '__main__':
+    main()
