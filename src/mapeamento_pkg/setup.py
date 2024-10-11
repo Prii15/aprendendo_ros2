23c3a1f9fb7a2b65c059d875ddd2d0185a2d0165
vmayres <victoroliveiraayres@gmail.com>
2024-10-11 01:11:16 -0300

    Remove unused console script 'lidar_to_grid_map'

--- src/mapeamento_pkg/setup.py
+++ src/mapeamento_pkg/setup.py
@@ -21,8 +21,6 @@ setup(
     entry_points={
         'console_scripts': [
             'lidar_to_grid = mapeamento_pkg.lidar_to_grid:main',
-            
-            'lidar_to_grid_map = mapeamento_pkg.lidar_to_grid_map:main',
         ],
     },
 )
