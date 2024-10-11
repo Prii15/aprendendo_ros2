ade83712b27519d96df51b1c967b481014ccd0a1
vmayres <victoroliveiraayres@gmail.com>
2024-10-11 01:05:39 -0300

    teste 1

--- src/mapeamento_pkg/setup.py
+++ src/mapeamento_pkg/setup.py
@@ -21,7 +21,8 @@ setup(
     entry_points={
         'console_scripts': [
             'lidar_to_grid = mapeamento_pkg.lidar_to_grid:main',
-            #'lidar_to_grid_map = mapeamento_pkg.lidar_to_grid_map:main',
+            
+            'lidar_to_grid_map = mapeamento_pkg.lidar_to_grid_map:main',
         ],
     },
 )
